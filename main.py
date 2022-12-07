6cec9816b00ce7f0ead1f6e136534197fd79327f
Я <veniaminovivan@yandex.ru>
2022-12-07 15:52:08 +0300

    reform to def

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
-from PIL import Image
+def return_can_move(schem, start, size, position='center', step=1):
 
-schem = 'image.png'
-start = 5, 5
-im = Image.open(schem)
-w, h = im.size
-pix = im.load()
-size = 10, 10
-position = 'center'
-x, y = start
-while True:
+    from PIL import Image
+
+    # schem = 'image.png'
+    # start = 5, 5
+    im = Image.open(schem)
+    w, h = im.size
+    pix = im.load()
+    # size = 10, 10
+    # position = 'center'
+    x, y = start
     a = input()
     if a == '>':
-        x += 1
+        x += step
     elif a == '<':
-        x -= 1
+        x -= step
     elif a == 'v':
-        y += 1
+        y += step
     elif a == '^':
-        y -= 1
+        y -= step
     x0 = (x - size[0] // 2) if position == 'center' else x - size[0]
     y0 = (y - size[1] // 2) if position == 'center' else y - size[1]
     x1 = (x + size[0] // 2) if position == 'center' else x
     y1 = (y + size[1] // 2) if position == 'center' else y
     f = True
-    for x2 in range(x0, x1 + 1):
-        for y2 in range(y0, y1 + 1):
+    for x2 in range(max(x0, 0), min(x1 + 1, w)):
+        for y2 in range(max(y0, 0), min(y1 + 1, h)):
             r, g, b = pix[x2, y2]
             if r / 16 > 15 and g / 16 > 15 and b / 16 > 15:
                 pass
@@ -35,14 +36,15 @@ while True:
             break
     if not f:
         if a == '>':
-            x -= 1
+            x -= step
         elif a == '<':
-            x += 1
+            x += step
         elif a == 'v':
-            y -= 1
+            y -= step
         elif a == '^':
-            y += 1
+            y += step
 
     print(x, y)
+    return x, y, (x, y) == start
 
 
--- main2.py
+++ main2.py
@@ -1,19 +1,28 @@
-from PIL import Image
-step = 5
-schem = 'image142.png'
-im = Image.open(schem)
-w, h = im.size
-pix = im.load()
-sheet = open('sheet.txt', 'w')
-for y in range(0, h - step + 1, step):
-    for x in range(0, w - step + 1, step):
-        n = 0
-        for i in range(step):
-            for j in range(step):
-                r, g, b, a = pix[x+i, y+j]
-                if r / 16 > 15 and g / 16 > 15 and b / 16 > 15:
-                    n += 1
-        n = round(n / step**2)
-        print(f'{n};', end='', file=sheet)
-    print('', file=sheet)
+def return_sheet(schem, sheet, step=1, wall=1, floor=0):
+
+    from PIL import Image
+    # step = 5
+    # schem = 'image142.png'
+    im = Image.open(schem)
+    w, h = im.size
+    pix = im.load()
+    # sheet = 'sheet.txt'
+    sheet = open(sheet, 'w')
+    for y in range(0, h - step + 1, step):
+        for x in range(0, w - step + 1, step):
+            n = 0
+            for i in range(step):
+                for j in range(step):
+                    r, g, b, a = pix[x+i, y+j]
+                    if r / 16 > 15 and g / 16 > 15 and b / 16 > 15:
+                        n += 1
+            n = round(n / step**2)
+            if not n:
+                n = wall
+            else:
+                n = floor
+            print(f'{n};', end='', file=sheet)
+        print('', file=sheet)
+
+
 
